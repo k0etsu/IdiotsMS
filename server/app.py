1be244279f7120d2852fcf38e7479e52fa4fc021
ryan <k0etsu@k0etsu.com>
2026-03-23 22:55:50 -0400

    fix kwargs

--- server/app.py
+++ server/app.py
@@ -146,8 +146,8 @@ class ChangePasswordSchema(Schema):
     confirmNewPassword = fields.Str(required=True)
 
     @validates('newPassword')
-    def validate_newPassword(self, value):
-        if value != self.context.get('confirmNewPassword'):
+    def validate_newPassword(self, value, data, **kwargs):
+        if value != data.get('confirmNewPassword'):
             raise ValidationError('New password confirmation does not match')
 
 # Validation helpers
